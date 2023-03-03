bd95df7d8c4182933297b9369acbdf2e90bb9521
vsvirydovich <v.sviridovich@imedia.by>
2023-03-03 23:39:46 +0300

    fix mask

--- app/scripts/components/maskForPhoneInput.js
+++ app/scripts/components/maskForPhoneInput.js
@@ -1,15 +1,17 @@
 import IMask from 'imask';
 
 const maskForPhoneInput = () => {
-  const phone = document.querySelector('.input-phone');
+  const phones = document.querySelectorAll('.input-phone');
 
-  if (!phone) return;
+  if (!phones.length) return;
 
-  IMask(
-    phone, {
-      mask: '+{7}(000)000-00-00',
-    },
-  );
+  phones.forEach((phone) => {
+    IMask(
+      phone, {
+        mask: '+{7}(000)000-00-00',
+      },
+    );
+  });
 };
 
 export default maskForPhoneInput;
